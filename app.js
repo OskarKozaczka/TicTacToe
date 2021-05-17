a01ef7394e1cfea9d0d6d9cbdbb1a7a8b95dbb51
OskarKozaczka <oskaro0@vp.pl>
2021-05-17 15:55:34 +0200

    initial

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => {
+	console.log(`app listening at http://localhost:${port}`);
+});
